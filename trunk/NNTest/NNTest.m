39f96b34ee75670176661975ed4e4df17401854b
patrick@fadeover.org <patrick@fadeover.org@7259528d-614a-0410-a9b5-7d53eb537e58>
2008-04-20 15:21:10 +0000

    Big improvement to the convergence rate of NN Test, still not perfect though

--- trunk/NNTest/NNTest.m
+++ trunk/NNTest/NNTest.m
@@ -9,17 +9,17 @@ int main (int argc, const char * argv[]) {
 	PBGNeuralNetwork *neuralNet = [[PBGNeuralNetwork alloc] initWithInputs:inputSize outputs:1 hiddenLayers:1];
 	
 	NSMutableArray *zeroZeroArray = [[NSMutableArray alloc] initWithCapacity:inputSize];
-	[zeroZeroArray addObject:[NSNumber numberWithInt:0]];
-	[zeroZeroArray addObject:[NSNumber numberWithInt:0]];
+	[zeroZeroArray addObject:[NSNumber numberWithDouble:0.000001]];
+	[zeroZeroArray addObject:[NSNumber numberWithDouble:0.000001]];
 	NSMutableArray *zeroOneArray = [[NSMutableArray alloc] initWithCapacity:inputSize];
-	[zeroOneArray addObject:[NSNumber numberWithInt:0]];
-	[zeroOneArray addObject:[NSNumber numberWithInt:1]];
+	[zeroOneArray addObject:[NSNumber numberWithDouble:0.000001]];
+	[zeroOneArray addObject:[NSNumber numberWithDouble:0.999999]];
 	NSMutableArray *oneZeroArray = [[NSMutableArray alloc] initWithCapacity:inputSize];
-	[oneZeroArray addObject:[NSNumber numberWithInt:1]];
-	[oneZeroArray addObject:[NSNumber numberWithInt:0]];
+	[oneZeroArray addObject:[NSNumber numberWithDouble:0.999999]];
+	[oneZeroArray addObject:[NSNumber numberWithDouble:0.000001]];
 	NSMutableArray *oneOneArray = [[NSMutableArray alloc] initWithCapacity:inputSize];
-	[oneOneArray addObject:[NSNumber numberWithInt:1]];
-	[oneOneArray addObject:[NSNumber numberWithInt:1]];
+	[oneOneArray addObject:[NSNumber numberWithDouble:0.999999]];
+	[oneOneArray addObject:[NSNumber numberWithDouble:0.999999]];
 	 
 	double sumSquaredError = 100;
 	int i = 1;
@@ -30,30 +30,35 @@ int main (int argc, const char * argv[]) {
 	
 	while (sumSquaredError > 0.01) {
 		[neuralNet setStartingValues:zeroZeroArray];
-		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:0]]];
+		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithDouble:0.000001]]];
 		double foo1 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-		error1 = 0 - foo1;
+		error1 = 0.000001 - foo1;
 		
 		[neuralNet setStartingValues:zeroOneArray];
-		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:1]]];
+		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithDouble:0.999999]]];
 		double foo2 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-		error2 = 1 - foo2;
+		error2 = 0.999999 - foo2;
 		
 		[neuralNet setStartingValues:oneZeroArray];
-		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:1]]];
+		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithDouble:0.999999]]];
 		double foo3 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-		error3 = 1 - foo3;
+		error3 = 0.999999 - foo3;
 		
 		[neuralNet setStartingValues:oneOneArray];
-		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:0]]];
+		[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithDouble:0.000001]]];
 		double foo4 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-		error4 = 0 - foo4;
+		error4 = 0.000001 - foo4;
 		
-		sumSquaredError = pow(error1, 2) + pow(error2, 2) + pow(error3, 2) + pow(error4, 2);
+		i++;
 		
-		NSLog(@"Sum Squared Error at epoch %d: %f", i++, sumSquaredError);
+		if (i % 1000 == 0) {
+			sumSquaredError = pow(error1, 2) + pow(error2, 2) + pow(error3, 2) + pow(error4, 2);
+			NSLog(@"Sum Squared Error at epoch %d: %f", i, sumSquaredError);
+		}
 		
-		if (i % 2000 == 0) {
+		
+		if (i % 100000 == 0) {
+			
 			[neuralNet printDescription];
 			
 			[neuralNet setStartingValues:zeroZeroArray];
@@ -70,65 +75,7 @@ int main (int argc, const char * argv[]) {
 		}			
 		
 	}
-	/*
-	
-	double _sumSquaredError = 100;
-	int j = 1;
-	
-	while (_sumSquaredError > 0.001) {
-		
-		sumSquaredError = 100;
-		while (sumSquaredError > 0.001) {
-			[neuralNet setStartingValues:zeroZeroArray];
-			[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:0]]];
-			double foo1 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-			error1 = 0 - foo1;
-			
-			sumSquaredError = pow(error1, 2);
-			
-			NSLog(@"Sum Squared Error at epoch %d: %f", i++, sumSquaredError);
-		}
-		
-		sumSquaredError = 100;
-		while (sumSquaredError > 0.001) {
-			[neuralNet setStartingValues:zeroOneArray];
-			[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:1]]];
-			double foo2 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-			error2 = 0 - foo2;
-			
-			sumSquaredError = pow(error2, 2);
-			
-			NSLog(@"Sum Squared Error at epoch %d: %f", i++, sumSquaredError);
-		}
-		
-		sumSquaredError = 100;
-		while (sumSquaredError > 0.001) {
-			[neuralNet setStartingValues:oneZeroArray];
-			[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:1]]];
-			double foo3 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-			error3 = 0 - foo3;
-			
-			sumSquaredError = pow(error3, 2);
-			
-			NSLog(@"Sum Squared Error at epoch %d: %f", i++, sumSquaredError);
-		}
-		
-		sumSquaredError = 100;
-		while (sumSquaredError > 0.001) {
-			[neuralNet setStartingValues:oneOneArray];
-			[neuralNet learnFromExpectedOutputs:[NSArray arrayWithObject:[NSNumber numberWithInt:0]]];
-			double foo4 = [[[neuralNet computeOutputValues] objectAtIndex:0] doubleValue];
-			error4 = 0 - foo4;
-			
-			sumSquaredError = pow(error4, 2);
-			
-			NSLog(@"Sum Squared Error at epoch %d: %f", i++, sumSquaredError);
-		}
-		
-		_sumSquaredError = pow(error1, 2) + pow(error2, 2) + pow(error3, 2) + pow(error4, 2);
-		NSLog(@"Sum Squared Error at epoch %d: %f", j++, sumSquaredError);
-	}
-	*/
+
 	[neuralNet setStartingValues:zeroZeroArray];
 	NSLog(@"Output for 0, 0: %@", [neuralNet outputValuesString]);
 	
@@ -141,6 +88,8 @@ int main (int argc, const char * argv[]) {
 	[neuralNet setStartingValues:oneOneArray];
 	NSLog(@"Output for 1, 1: %@", [neuralNet outputValuesString]);
 
+	[neuralNet printDescription];
+	
     [pool drain];
     return 0;
 }
